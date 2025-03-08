4663e8d6cd74935d601d99a65a956f3a72d0daff
Anna K <1415ks@ukr.net>
2025-03-09 00:06:46 +0200

    add html,css,js

---
+++ src/js/faq.js
@@ -0,0 +1,12 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordion = new Accordion('.faq-item', {
+    elementClass: 'faq-item',
+    triggerClass: 'faq-question',
+    panelClass: 'faq-answer',
+    activeClass: 'open',
+    duration: 300, // Тривалість анімації
+  });
+});
