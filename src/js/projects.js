2a751fd5083de8e2bac1a1c7697a944db78d2492
Andrii Semenenko <oxonomy14@gmail.com>
2025-03-08 22:43:06 +0700

    add jsv1

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,10 +1,22 @@
 import Swiper from 'swiper';
 import 'swiper/css/bundle';
 
+const elemProjectBtnNext = document.querySelector('.swiper-project-btn-next');
+const elemProjectBtnPrev = document.querySelector('.swiper-project-btn-prev');
+
 const swiper = new Swiper('.swiper', {
   speed: 400,
   spaceBetween: 100,
 });
 
-// Now you can use all slider methods like
-//swiper.slideNext();
+elemProjectBtnNext.addEventListener('click', onBtnNext);
+
+function onBtnNext() {
+  swiper.slideNext();
+}
+
+elemProjectBtnPrev.addEventListener('click', onBtnPrev);
+
+function onBtnPrev() {
+  swiper.slidePrev();
+}
