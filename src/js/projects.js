fd9e4980ca8210086bf031d9b1efb355f16bf67f
Andrii Semenenko <oxonomy14@gmail.com>
2025-03-09 04:53:05 +0700

    add jsv2

--- src/js/projects.js
+++ src/js/projects.js
@@ -5,18 +5,53 @@ const elemProjectBtnNext = document.querySelector('.swiper-project-btn-next');
 const elemProjectBtnPrev = document.querySelector('.swiper-project-btn-prev');
 
 const swiper = new Swiper('.swiper', {
-  speed: 400,
-  spaceBetween: 100,
+  speed: 900,
+  spaceBetween: 400,
+  on: {
+    slideChange: updateButtonsState, // Викликаємо функцію при зміні слайду
+  },
 });
 
-elemProjectBtnNext.addEventListener('click', onBtnNext);
+// Функція для оновлення стану кнопок
+function updateButtonsState() {
+  if (swiper.isBeginning) {
+    elemProjectBtnPrev.classList.add('swiper-project-btn-disabled');
+    elemProjectBtnPrev.setAttribute('disabled', 'true');
+  } else {
+    elemProjectBtnPrev.classList.remove('swiper-project-btn-disabled');
+    elemProjectBtnPrev.removeAttribute('disabled');
+  }
 
-function onBtnNext() {
+  if (swiper.isEnd) {
+    elemProjectBtnNext.classList.add('swiper-project-btn-disabled');
+    elemProjectBtnNext.setAttribute('disabled', 'true');
+  } else {
+    elemProjectBtnNext.classList.remove('swiper-project-btn-disabled');
+    elemProjectBtnNext.removeAttribute('disabled');
+  }
+}
+
+elemProjectBtnNext.addEventListener('click', evt => {
   swiper.slideNext();
-}
+  updateButtonsState(); // Оновлюємо стан кнопок
+});
 
-elemProjectBtnPrev.addEventListener('click', onBtnPrev);
-
-function onBtnPrev() {
+elemProjectBtnPrev.addEventListener('click', evt => {
   swiper.slidePrev();
-}
+  updateButtonsState(); // Оновлюємо стан кнопок;
+});
+
+window.addEventListener('keydown', evt => {
+  if (evt.code === 'ArrowRight') {
+    swiper.slideNext();
+    updateButtonsState(); // Оновлюємо стан кнопок
+    console.log('Натискається клавіша Вправо');
+  } else if (evt.code === 'ArrowLeft') {
+    swiper.slidePrev();
+    updateButtonsState(); // Оновлюємо стан кнопок;
+    console.log('Натискається клавіша Вліво');
+  }
+});
+
+// Викликаємо функцію один раз, щоб встановити правильний стан кнопок на початку
+updateButtonsState();
