7fc5c94ce4979f9b4eb5bc31818dfd410930a0a8
Andrii Semenenko <oxonomy14@gmail.com>
2025-03-08 15:57:22 +0700

    st_1557_080325

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,20 +1,10 @@
 import Swiper from 'swiper';
 import 'swiper/css/bundle';
-//import { SwiperOptions } from 'swiper/types';
 
 const swiper = new Swiper('.swiper', {
   speed: 400,
   spaceBetween: 100,
 });
-
-// const swiperParams: SwiperOptions = {
-//   speed: 400,
-//   spaceBetween: 100,
-// };
-
-//const swiper = new Swiper('.swiper');
-
-//const swiper = document.querySelector('.swiper').swiper;
 
 // Now you can use all slider methods like
 //swiper.slideNext();
